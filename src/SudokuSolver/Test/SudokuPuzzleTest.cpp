2243ccbec05e7829b88ac0007511c42040f8bb01
Evan Loughlin <eloughlin3@gatech.edu>
2019-10-16 17:15:30 -0600

    GoogleTest working with CMake

--- src/SudokuSolver/Test/SudokuPuzzleTest.cpp
+++
@@ -1,8 +0,0 @@
-// #include "SudokuPuzzle.h"
-// #include <gtest/gest.h>
-
-// int main(int argc, char* argv[])
-// {
-//     testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
---
+++ test/TestSudokuPuzzle.cpp
@@ -0,0 +1,8 @@
+
+#include "gtest/gtest.h"
+#include "SudokuPuzzle.h"
+
+TEST(SudokuPuzzle, canBeInstantiated)
+{
+    ASSERT_FALSE(false);
+}
