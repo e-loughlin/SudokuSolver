1074381a984412eb16c130a484b9303203503230
Evan Loughlin <eloughlin3@gatech.edu>
2019-10-16 18:10:07 -0600

    Introduced boilerplate test fixture for SudokuPuzzle class. (Currently experiencing compilation issues)

--- test/TestSudokuPuzzle.cpp
+++ test/TestSudokuPuzzle.cpp
@@ -1,8 +1,32 @@
 
 #include "gtest/gtest.h"
+#include <QScopedPointer>
 #include "SudokuPuzzle.h"
 
-TEST(SudokuPuzzle, canBeInstantiated)
+namespace 
 {
-    ASSERT_FALSE(false);
-}
+    class TestSudokuPuzzle : public ::testing::test 
+    {
+     protected:
+        TestSudokuPuzzle(){}
+        ~TestSudokuPuzzle() override {}
+
+        void SetUp() override
+        {
+            mPatient.reset(new SudokuPuzzle());
+        }           
+
+        void TearDown() override
+        {
+            mPatient.reset();
+        }
+
+        QScopedPointer<SudokuPuzzle> mPatient;
+    };
+
+    TEST_F(TestSudokuPuzzle, CanBeInstantiated)
+    {
+        ASSERT_FALSE(false);
+    }
+    
+} //namespace
