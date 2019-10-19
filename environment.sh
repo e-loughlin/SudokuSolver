ea8050757bf9a32c2c64ca1fd038c83b8b920c5d
Evan Loughlin <emloughl@gmail.com>
2019-10-19 00:48:57 -0600

    Added alias in environment.sh for calling NewClass.py script

--- environment.sh
+++ environment.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-#Get current script directory
+################################
+# Development Environment Setup
+################################
+
+# Download dependencies
+git submodule update --init --recursive
+
+# Get current script directory
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Set alias for building from source directory and tests
 alias b="python3 ${DIR}/tools/build_current_directory.py"
+
+# Set alias for new_class.py script
+alias newclass="python3 ${DIR}/tools/CplusplusCodeGenerators/src/NewClass.py"
