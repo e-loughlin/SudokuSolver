5e788dbbf156a89fdf4a497d2f1925486e54e46f
Evan Loughlin <emloughl@gmail.com>
2019-10-19 02:29:32 -0600

    Updated build_current_directory.py to handle clean rebuilds, and non-existent build directory

--- tools/build_current_directory.py
+++ tools/build_current_directory.py
@@ -8,6 +8,7 @@
 import argparse
 from pathlib import Path
 import os
+import git
 
 def stringToBool(v):
     if isinstance(v, bool):
@@ -20,7 +21,9 @@ def stringToBool(v):
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 argumentParser = argparse.ArgumentParser(description='Build settings.')
-argumentParser.add_argument('-notests', '--notests', '-notb', '--notb' dest='notests', type=stringToBool, nargs='?', const=True, default=False, help="Disables building tests.")
+argumentParser.add_argument('-notests', '--notests', '-notb', '--notb', dest='notests', type=stringToBool, nargs='?', const=True, default=False, help="Disables building tests.")
+argumentParser.add_argument('-c', '--clean', dest='clean', type=stringToBool, nargs='?', const=True, default=False, help="Cleans build from current working directory.")
+argumentParser.add_argument('-ca', '--cleanall', dest='clean_all', type=stringToBool, nargs='?', const=True, default=False, help="Cleans build directory.")
 
 def run_tests():
     # Get same directory as current, but within /build
@@ -32,29 +35,74 @@ def run_tests():
         if os.path.basename(filename) == "run_tests":
             os.system(filename)
 
-def build():
-    # Get same directory as current, but within /build
+def build_dir_exists():
+    return os.path.isdir(get_build_dir())
+
+def make_build_dir():
+    os.system("cd {0} && mkdir build && cd {1}".format(get_git_root(), os.getcwd()))
+
+def run_cmake_at_root():
+    cwd = os.getcwd()
+    os.system("cd {0} && cmake ../ && cd {1}".format(get_build_dir(), cwd))
+
+def run_make_at_root():
+    cwd = os.getcwd()
+    os.system("cd {0} && make && cd {1}".format(get_build_dir(), cwd))
+
+def clean_rebuild():
+    os.system("cd {0} && rm -rf * && cd {1}".format(get_git_root(), os.getcwd()))
+    run_cmake_at_root()
+    run_make_at_root()
+
+def get_git_root():
+    git_repo = git.Repo(os.getcwd(), search_parent_directories=True)
+    git_root = git_repo.git.rev_parse("--show-toplevel")
+    print(git_root)
+    return git_root
+
+def get_src_dir():
+    return get_git_root() + "/src"
+
+def get_build_dir():
+    return get_git_root() + "/build"
+
+def build_from_src_dir():
+    # Get same directory as current, but within /build (#TODO: Make this less hacky)
     cwd = os.getcwd()
     buildCwd = cwd.replace("/src", "/build/src")
     
     os.system("cd {0} && make -b && cd {1}".format(buildCwd, cwd))
 
-def build_directory_exists()
-
+def get_user_permission(warningMessage):
+    print("Warning:\n\t{0}\n\nContinue? (y/n)".format(warningMessage))
+    return stringToBool(input())
 
 def cwd_in_src():
     return ("build/src" not in os.getcwd()) and ("/src" in os.getcwd()) 
 
 def main():
+    # Parse arguments
+    args = argumentParser.parse_args()
+
     if not cwd_in_src():
         print("Build script can only be run within source directory (source).")
         exit()
+        
+    if not (build_dir_exists()):
+        make_build_dir()
+        clean_rebuild()
+        exit()
+
+
+    if(args.clean_all):
+        if(get_user_permission("Clean *entire* /build directory?")):
+            clean_rebuild()
+        exit()
 
     # Build recursively
-    build()
+    build_from_src_dir()
 
     # Run all tests
-    args = argumentParser.parse_args()
     if(not args.notests):
         run_tests()
 
