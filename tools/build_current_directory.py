025b97522b99628904c45bf1d780f0b965f7d354
Evan Loughlin <emloughl@gmail.com>
2019-10-18 19:12:50 -0600

    Began script for auto-running cmake and built tests recursively downward from current directory.

---
+++ tools/build_current_directory.py
@@ -0,0 +1,13 @@
+# Script for building current directory, and running tests.
+
+import argparse
+
+argumentParser = argparse.ArgumentParser(description='Build settings.')
+argumentParser.add_argument('')
+
+def main():
+
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
