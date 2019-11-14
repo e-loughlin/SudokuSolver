479ad7af2a50aef05f1c3c65b486dec7cdf188d7
Evan Loughlin <evan.loughlin@circlecvi.com>
2019-11-14 13:20:09 -0700

    Integrated CppCodeGenerator into project

--- environment.sh
+++ environment.sh
@@ -11,7 +11,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 alias b="python3 ${DIR}/tools/build_current_directory.py"
 
 # Set alias for new_class.py script
-alias newclass="python3 ${DIR}/tools/CplusplusCodeGenerators/src/NewClass.py"
-
-# Set alias for gmock_generator
-alias gmock_gen="python3 ${DIR}/extern/googletest/googlemock/scripts/generator/gmock_gen.py"
+alias newclass="${DIR}/tools/CppCodeGenerator/build/main"
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,7 +6,16 @@ sudo apt install python3-pip
 pip3 install gitpython
 sudo apt install cmake
 
+#Go Dependency
+
 #Qt5
 sudo apt-get install qt5-default
 sudo apt-get install qtbase5-dev
 sudo apt-get install qtdeclarative5-dev
+
+#CppCodeGenerator install
+cd tools/CppCodeGenerator
+rm -rf build
+mkdir build
+cd build
+go build ../main.go
