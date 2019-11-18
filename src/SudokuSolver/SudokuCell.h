cf220d0417c8206267c89ba6a02eb4227257ca27
Evan Loughlin <evan.loughlin@circlecvi.com>
2019-11-18 15:28:42 -0700

    Simplified ENUM, as working with it was unnecessarily complicated

--- src/SudokuSolver/SudokuCell.h
+++ src/SudokuSolver/SudokuCell.h
@@ -3,9 +3,7 @@
    
    Author: Evan Loughlin   
 
-   Description: Enumeration of possible values within a Sudoku Cell. Each cell 
-   can contain one of 9 values, or blank. Implemented as bit flags in order to 
-   utilize bitwise operations.
+   Description: Enumeration of possible values within a Sudoku Cell.
 */
 
 #ifndef SUDOKUCELL_H
@@ -14,15 +12,15 @@
 enum SudokuCell 
 {
     BLANK = 0,
-    _1 = 1 << 0, // Binary: 000000001
-    _2 = 1 << 1, // Binary: 000000010
-    _3 = 1 << 2, // Binary: 000000100
-    _4 = 1 << 3, // Binary: 000001000
-    _5 = 1 << 4, // Binary: 000010000
-    _6 = 1 << 5, // Binary: 000100000
-    _7 = 1 << 6, // Binary: 001000000
-    _8 = 1 << 7, // Binary: 010000000
-    _9 = 1 << 8, // Binary: 100000000
+    _1 = 1,
+    _2 = 2,
+    _3 = 3,
+    _4 = 4,
+    _5 = 5,
+    _6 = 6,
+    _7 = 7,
+    _8 = 8,
+    _9 = 9
 };
 
 #endif // SUDOKUCELL_H
