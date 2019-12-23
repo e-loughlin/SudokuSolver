3ca38661b772867eb1fe88422a3eaa62418a5afa
Evan Loughlin <emloughl@gmail.com>
2019-12-22 23:19:42 -0700

    Introduced SudokuPuzzleValidator interface

---
+++ src/SudokuSolver/I_SudokuPuzzleValidator.h
@@ -0,0 +1,19 @@
+/*
+   @file I_SudokuPuzzleValidator.h
+
+   Copyright (C) Evan Loughlin 2019
+*/
+ 
+#pragma once
+
+class SudokuPuzzle;
+
+class I_SudokuPuzzleValidator 
+{
+ public:
+    virtual ~I_SudokuPuzzleValidator(){}
+
+ public:
+    virtual bool isValid(SudokuPuzzle puzzle) = 0;
+    
+};
