74b0f381c1aa4b9ecb46d48e0c2b56bd86cb42dc
Evan Loughlin <emloughl@gmail.com>
2020-12-29 23:35:17 -0700

    Update environment.sh for correct CppCodeGenerator sourcing

--- environment.sh
+++ environment.sh
@@ -10,5 +10,9 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Set alias for building from source directory and tests
 alias b="python3 ${DIR}/tools/build_current_directory.py"
 
-# Set alias for new_class.py script
+# Build and set alias for CppCodeGenerator
+mkdir ${DIR}/tools/CppCodeGenerator/build/
+cd ${DIR}/tools/CppCodeGenerator/build/
+go build ${DIR}/tools/CppCodeGenerator/
+cd ${DIR}
 alias codegen="${DIR}/tools/CppCodeGenerator/build/CppCodeGenerator"
